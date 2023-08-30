46ac043ec3b0049fbecd01e954003a7308c7f667
Joseph Osborne <jd.osborne5@icloud.com>
2023-08-30 14:52:17 +0100

    Include helpful auto-launch for browser

--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"embed"
 	"log"
 	"net/http"
+	"github.com/pkg/browser"
 )
 
 //go:embed *.html
 var files embed.FS
 
 func main() {
-	handler := new(handler)
-	log.Fatal(http.ListenAndServe(":8081", handler))
+	exit := make(chan int, 1)
+	go func() {
+		handler := new(handler)
+		log.Fatal(http.ListenAndServe(":8081", handler))
+		exit <- 1
+	}()
+
+	err := browser.OpenURL("http://localhost:8081")
+	if err != nil {
+		log.Print(err)
+	}
+
+	<- exit
 }
